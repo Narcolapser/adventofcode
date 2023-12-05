e01e09541e456e85dc1c93b1dc4bcb13b0b596ef
toby <toby@studiosleepygiraffe.com>
2023-12-04 18:12:04 -0600

    Day 4 part 2 complete.

--- 2023/d04.py
+++ 2023/d04.py
@@ -10,4 +10,30 @@ for line in lines:
     if matches:
         pile_sum += 2**(matches-1)
 
+print(f'answer 1: pile_sum')
+
+# Answer 2
+cards = {}
+for i,line in enumerate(lines):
+    winners, numbers = line.split(':')[1].split('|')
+    winners = {int(i) for i in winners.split(' ') if i.isdigit()}
+    numbers = {int(i) for i in numbers.split(' ') if i.isdigit()}
+    matches = len(numbers.intersection(winners))
+    cards[i] = {'matches':matches,'worth': 2**(matches-1) if matches else 0}
+
+pile_sum = 0
+# Now recurse through the deck finding value
+def copies_value(pos):
+    if pos >= len(cards) - 1:
+        return 0
+    value = 1
+    for i in range(cards[pos]['matches']):
+        value += copies_value(pos+i+1)
+    return value
+
+for c, card in enumerate(cards):
+    pile_sum += 1
+    for i in range(cards[card]['matches']):
+        pile_sum += copies_value(c+i+1)
+
 print(pile_sum)
