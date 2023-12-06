cc05ac066625cf19603018d766ff8af6ba9f87c5
Toby Archer <Toben.Archer@CovenantEyes.com>
2023-12-06 17:58:06 -0600

    day 5 tweaks.

--- 2023/d05.py
+++ 2023/d05.py
@@ -3,14 +3,22 @@
 # If it is inside your range, what does it map to? 
 # If none of the range objects identify with the value, then we know it is a straight pass through.
 
-segments = open('d05.sample').read().split('\n\n')
+#segments = open('d05.sample').read().split('\n\n')
 segments = open('d05.input').read().split('\n\n')
 
-seeds = [int(i) for i in segments[0].split(':')[1].split(' ') if i.isdigit()]
+#seeds = [int(i) for i in segments[0].split(':')[1].split(' ') if i.isdigit()]
+seed_digits = [int(i) for i in segments[0].split(':')[1].split(' ') if i.isdigit()]
+seeds = []
+while len(seed_digits):
+    start = seed_digits.pop(0)
+    seed_range = seed_digits.pop(0)
+    for i in range(seed_range):
+        seeds.append(start+i)
 
+print(len(seeds))
 class Range():
     def __init__(self,range_line):
-        self.destination, self.source, self.range = [int(i) for i in range_line.split(' ')]
+        self.destination, self.source, self.range = [int(i) for i in range_line.split(' ') if i.isdigit()]
 
     def __contains__(self,value):
         if value < self.source:
@@ -37,7 +45,7 @@ class RangeMap():
 
 maps = []
 for segment in segments[1:]:
-    ranges = [Range(line) for line in segment.split('\n')[1:]]
+    ranges = [Range(line) for line in segment.split('\n')[1:] if ' ' in line]
     maps.append(RangeMap(ranges))
 
 locations = []
