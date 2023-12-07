fa9acf6b7458804e7b67a99cef8fb5278bcfe69d
toby <toby@studiosleepygiraffe.com>
2023-12-06 19:47:02 -0600

    Progress on Day 5. I think I'll need to revisit tomorrow yet.

--- 2023/d05.py
+++ 2023/d05.py
@@ -1,21 +1,5 @@
-# lets break this up logically. Let's make range objects.
-# Given a value, can you identify whether that value is inside your range?
-# If it is inside your range, what does it map to? 
-# If none of the range objects identify with the value, then we know it is a straight pass through.
+MAX_INT = 10**40 # Python has no max int. I just need an absurdly large number.
 
-#segments = open('d05.sample').read().split('\n\n')
-segments = open('d05.input').read().split('\n\n')
-
-#seeds = [int(i) for i in segments[0].split(':')[1].split(' ') if i.isdigit()]
-seed_digits = [int(i) for i in segments[0].split(':')[1].split(' ') if i.isdigit()]
-seeds = []
-while len(seed_digits):
-    start = seed_digits.pop(0)
-    seed_range = seed_digits.pop(0)
-    for i in range(seed_range):
-        seeds.append(start+i)
-
-print(len(seeds))
 class Range():
     def __init__(self,range_line):
         self.destination, self.source, self.range = [int(i) for i in range_line.split(' ') if i.isdigit()]
@@ -33,27 +17,65 @@ class Range():
             return False
         return self.destination + (value - self.source )
 
+    def map_extended(self,value):
+        if value not in self:
+            return False
+        mapping = self.destination + (value - self.source )
+        left = self.range - (value - self.source) -1
+        return {'map':mapping, 'more':left}
+
 class RangeMap():
-    def __init__(self, ranges):
+    def __init__(self, name, ranges, next_range):
+        self.name = name
         self.ranges = ranges
+        self.low_points = {r.source:r for r in ranges}
+        self.next_range = next_range
     
     def map(self,value):
         for range in self.ranges:
             if value in range:
                 return range.map(value)
         return value
+    
+    def map_range(self,value):
+        for range in self.ranges:
+            if value in range:
+                return range.map_extended(value)
+        # If we are here none of our ranges match. But we need to find the when the next range will pickup.
+        finders = [r.source-value for r in self.ranges if r.source-value > 0]
+        more = min(finders) if len(finders) else MAX_INT
+        return {'map':value, 'more':more}
+    
+    def map_extended(self,value):
+        ''' given value, what does it map to and how long will it stay that way?'''
+        result = self.map_range(value)
+        value = result['map']
+        more = result['more']
+        if self.next_range:
+            lower = self.next_range.map_extended(value)
+            value = lower['map']
+            more = min([lower['more'],more])
+        return {'map':value, 'more':more}
 
-maps = []
-for segment in segments[1:]:
-    ranges = [Range(line) for line in segment.split('\n')[1:] if ' ' in line]
-    maps.append(RangeMap(ranges))
+if __name__ == '__main__':
+    #segments = open('d05.sample').read().split('\n\n')
+    segments = open('d05.input').read().split('\n\n')
 
-locations = []
+    #seeds = [int(i) for i in segments[0].split(':')[1].split(' ') if i.isdigit()]
+    seed_digits = [int(i) for i in segments[0].split(':')[1].split(' ') if i.isdigit()]
+    seed_ranges = []
+    while len(seed_digits):
+        start = seed_digits.pop(0)
+        seed_range = seed_digits.pop(0)
+        seed_ranges.append((start,seed_range))
 
-for seed in seeds:
-    value = seed
-    for rmap in maps:
-        value = rmap.map(value)
-    locations.append(value)
+    maps = []
+    for segment in segments[1:]:
+        ranges = [Range(line) for line in segment.split('\n')[1:] if ' ' in line]
+        maps.append(RangeMap(ranges))
 
-print(min(locations))
+    # given a seed range start from it's lowest point, map that.
+    # Now find the start of the first range. Skip every number between the lowest and the start
+    # Find that range's lowest point and map that. Skip the remaining mappings.
+    # Map the start of the next un-mapped range to the bottom of the next range.
+    # Repeat this until we get to the end of the seed range.
--- 2023/d05.test.py
+++ 2023/d05.test.py
@@ -1,7 +1,24 @@
 import unittest
-from d05 import Range, RangeMap
+from d05 import Range, RangeMap, MAX_INT
 
 class Test_Range(unittest.TestCase):
+    def setUp(self):
+        f = open('d05.sample')
+        segments = f.read().split('\n\n')
+        f.close()
+        maps = []
+        for segment in segments[1:]:
+            name = segment.split('\n')[0]
+            ranges = [Range(line) for line in segment.split('\n')[1:] if ' ' in line]
+            maps.append(RangeMap(name,ranges,None))
+
+        self.rmap = maps.pop(0)
+        cursor = self.rmap
+        while len(maps):
+            cursor.next_range = maps.pop(0)
+            cursor = cursor.next_range
+        
+
     def test_line_parse(self):
         r = Range('50 98 2')
         self.assertEqual(r.destination, 50)
@@ -28,21 +45,57 @@ class Test_Range(unittest.TestCase):
 
     def test_mapping(self):
         r = Range('52 50 48')
-        self.assertEquals(r.map(50),52)
-        self.assertEquals(r.map(51),53)
-        self.assertEquals(r.map(96),98)
-        self.assertEquals(r.map(97),99)
+        self.assertEqual(r.map(50),52)
+        self.assertEqual(r.map(51),53)
+        self.assertEqual(r.map(96),98)
+        self.assertEqual(r.map(97),99)
         self.assertFalse(r.map(98))
 
+    def test_mapping_extended(self):
+        r = Range('52 50 48')
+        self.assertFalse(r.map_extended(49))
+        self.assertEqual(r.map_extended(50),{'map':52,'more':47})
+        self.assertEqual(r.map_extended(51),{'map':53,'more':46})
+        self.assertEqual(r.map_extended(96),{'map':98,'more':1})
+        self.assertEqual(r.map_extended(97),{'map':99,'more':0})
+        self.assertFalse(r.map_extended(98))
+
     def test_range_map(self):
-        rm = RangeMap([Range('50 98 2'), Range('52 50 48')])
-        self.assertEquals(rm.map(1),1)
-        self.assertEquals(rm.map(49),49)
-        self.assertEquals(rm.map(51),53)
-        self.assertEquals(rm.map(96),98)
-        self.assertEquals(rm.map(97),99)
-        self.assertEquals(rm.map(98),50)
-        self.assertEquals(rm.map(99),51)
+        rm = RangeMap('test',[Range('50 98 2'), Range('52 50 48')],None)
+        self.assertEqual(rm.map(1),1)
+        self.assertEqual(rm.map(49),49)
+        self.assertEqual(rm.map(51),53)
+        self.assertEqual(rm.map(96),98)
+        self.assertEqual(rm.map(97),99)
+        self.assertEqual(rm.map(98),50)
+        self.assertEqual(rm.map(99),51)
+
+    def test_range_map_range(self):
+        rm = RangeMap('test',[Range('50 98 2'), Range('52 50 48')],None)
+        self.assertEqual(rm.map_range(1),{'map':1,'more':49})
+        self.assertEqual(rm.map_range(49),{'map':49,'more':1})
+        self.assertEqual(rm.map_range(50),{'map':52,'more':47})
+        self.assertEqual(rm.map_range(51),{'map':53,'more':46})
+        self.assertEqual(rm.map_range(96),{'map':98,'more':1})
+        self.assertEqual(rm.map_range(97),{'map':99,'more':0})
+        self.assertEqual(rm.map_range(98),{'map':50,'more':1})
+        self.assertEqual(rm.map_range(99),{'map':51,'more':0})
+        self.assertEqual(rm.map_range(100),{'map':100,'more':MAX_INT})
+        
+
+    def test_range_linking(self):
+        cursor = self.rmap
+        count = 1
+        while cursor.next_range:
+            cursor = cursor.next_range
+            count += 1
+        self.assertEqual(count,7)
+
+    def test_range_map_extended(self):
+        self.assertEqual(self.rmap.map_extended(79)['map'],82)
+        self.assertEqual(self.rmap.map_extended(14)['map'],43)
+        self.assertEqual(self.rmap.map_extended(55)['map'],86)
+        self.assertEqual(self.rmap.map_extended(13)['map'],35)
 
 if __name__ == '__main__':
     unittest.main()
