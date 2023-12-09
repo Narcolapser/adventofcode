6a1014dafa5ff237f903f6b51ed5689f483932b4
toby <toby@studiosleepygiraffe.com>
2023-12-09 12:09:19 -0600

    Day 9. Not sure where I'm broken.

---
+++ 2023/09/main.py
@@ -0,0 +1,63 @@
+class Row():
+    def __init__(self,row):
+        self.values = [int(i) for i in row.split(' ') if i.isdigit()]
+    
+    def predict(self,value):
+        if sum(self.values) == 0:
+            return 0
+        else:
+            return self.values[-1] + value
+    
+    def __eq__(self, other):
+        if len(self.values) != len(other.values):
+            return False
+        
+        for i,value in enumerate(self.values):
+            if value != other.values[i]:
+                return False
+        
+        return True
+
+    def get_descendant(self):
+        if sum(self.values) == 0:
+            return False
+        
+        if len(self.values) == 1:
+            return Row('0')
+        
+        previous = self.values[0]
+        new_values = []
+        for i in self.values[1:]:
+            new_values.append(str(i - previous))
+            previous = i
+
+        return Row(' '.join(new_values))
+
+    def expand(self):
+        if sum(self.values) == 0:
+            self.values.append(0)
+            return 0
+
+        descendant = self.get_descendant()
+        descendant.expand()
+        next_value = self.predict(descendant.values[-1])
+        self.values.append(next_value)
+        return next_value
+    
+    def __str__(self):
+        return ' '.join([str(val) for val in self.values])
+    
+    def __repr__(self):
+        return str(self)
+
+if __name__ == '__main__':
+    #lines = open('sample').read().split('\n')
+    lines = open('input').read().split('\n')
+    rows = [Row(line) for line in lines]
+    print(f'Number of rows: {len(rows)}')
+    increment = 0
+    for row in rows:
+        next_value = row.expand()
+        increment += next_value
+    
+    print(f'historic increment: {increment}')
---
+++ 2023/09/test.py
@@ -0,0 +1,66 @@
+import unittest
+from main import Row
+
+class TestRow(unittest.TestCase):
+    def test_predict_next_0(self):
+        r = Row('0 0 0 0')
+        predicted = r.predict(5)
+        self.assertEqual(predicted,0)
+
+    def test_predict_non_zero(self):
+        r = Row('2 2 2 2')
+        predicted = r.predict(0)
+        self.assertEqual(predicted, 2)
+
+        r = Row('0 2 4 6')
+        predicted = r.predict(2)
+        self.assertEqual(predicted, 8)
+
+        r = Row('3 3 5 9 15')
+        predicted = r.predict(8)
+        self.assertEqual(predicted, 23)
+
+        r = Row('10 13 16 21 30 45')
+        predicted = r.predict(23)
+        self.assertEqual(predicted, 68)
+    
+    def test_compare_row(self):
+        r1 = Row('2 2 2 2')
+        r2 = Row('2 2 2 2')
+        r3 = Row('1 2 3 4')
+
+        self.assertTrue(r1 == r2)
+        self.assertFalse(r2 == r3)
+        self.assertFalse(r1 == r3)
+    
+    def test_get_next_row(self):
+        r = Row('10 13 16 21 30 45')
+        decendant = r.get_descendant()
+        self.assertTrue(Row('3 3 5 9 15') == decendant)
+
+        r = Row('1   3   6  10  15  21')
+        decendant = r.get_descendant()
+        next_row = Row('2   3   4   5   6')
+        self.assertTrue(decendant == next_row)
+        
+        r = next_row
+        decendant = r.get_descendant()
+        next_row = Row('1  1 1 1')
+        self.assertTrue(decendant == next_row)
+
+        r = next_row
+        decendant = r.get_descendant()
+        next_row = Row('0 0 0')
+        self.assertTrue(decendant == next_row)
+
+        self.assertFalse(next_row.get_descendant())
+    
+    def test_expand(self):
+        start = Row('0   3   6   9  12  15')
+        end = Row('0   3   6   9  12  15  18')
+        expand_value = start.expand()
+        self.assertEqual(expand_value, 18)
+        self.assertTrue(end == start)
+
+if __name__ == '__main__':
+    unittest.main()
