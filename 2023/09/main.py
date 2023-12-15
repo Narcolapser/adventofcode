c4a2e38088e6ed707738336b20187019d6b2e66d
toby <toby@studiosleepygiraffe.com>
2023-12-14 19:04:37 -0600

    another 7 minutes to day 9, but my head is to foggy yet.

--- 2023/09/main.py
+++ 2023/09/main.py
@@ -1,6 +1,7 @@
 class Row():
     def __init__(self,row):
         self.values = [int(i) for i in row.split(' ') if i.isdigit()]
+        self.descendant = None
     
     def predict(self,value):
         if sum(self.values) == 0:
@@ -19,19 +20,24 @@ class Row():
         return True
 
     def get_descendant(self):
-        if sum(self.values) == 0:
-            return False
-        
-        if len(self.values) == 1:
-            return Row('0')
-        
-        previous = self.values[0]
-        new_values = []
-        for i in self.values[1:]:
-            new_values.append(str(i - previous))
-            previous = i
+        if self.descendant:
+            return self.descendant
+        else:
+            if sum(self.values) == 0:
+                return False
+            
+            if len(self.values) == 1:
+                return Row('0')
+            
+            previous = self.values[0]
+            new_values = []
+            for i in self.values[1:]:
+                new_values.append(str(i - previous))
+                previous = i
 
-        return Row(' '.join(new_values))
+            self.descendant = Row(' '.join(new_values))
+            return self.descendant
+            
 
     def expand(self):
         if sum(self.values) == 0:
@@ -39,26 +45,36 @@ class Row():
             return 0
 
         descendant = self.get_descendant()
+        stack.append(descendant)
         descendant.expand()
         next_value = self.predict(descendant.values[-1])
         self.values.append(next_value)
         return next_value
     
     def __str__(self):
-        return ' '.join([str(val) for val in self.values])
+        return '  '.join([str(val) for val in self.values])
     
     def __repr__(self):
         return str(self)
 
+stack = []
+
 if __name__ == '__main__':
     #lines = open('sample').read().split('\n')
-    lines = open('input').read().split('\n')
+    #lines = open('input').read().split('\n')
+    lines = open('single').read().split('\n')
     rows = [Row(line) for line in lines]
     print(f'Number of rows: {len(rows)}')
     increment = 0
+    stack.append(rows[0])
     for i,row in enumerate(rows):
         next_value = row.expand()
         print(f'row: {i} extrapolated value of {next_value}')
         increment += next_value
-    
+    #stack.append(rows[0])
     print(f'historic increment: {increment}')
+    py = open('pyramid').read().split('\n')
+    #for i,row in enumerate(stack[::-1]):
+    for i,row in enumerate(stack):
+        print(py[i])
+        print(f'{i+1}.  {row}')
