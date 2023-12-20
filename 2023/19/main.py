99b8745d076158f9d02c6a4c82dda553845e45ce
toby <toby@studiosleepygiraffe.com>
2023-12-19 19:23:01 -0600

    setup complete after 48 minutes. on to work.

---
+++ 2023/19/main.py
@@ -0,0 +1,70 @@
+class Part:
+    x = None
+    m = None
+    a = None
+    s = None
+    visited: set[str] = None
+    def __init__(self,line):
+        x,m,a,s = line.strip('{').strip('}').split(',')
+        self.x = int(x.split('=')[1])
+        self.m = int(m.split('=')[1])
+        self.a = int(a.split('=')[1])
+        self.s = int(s.split('=')[1])
+        self.visited = set()
+
+class Workflow:
+    name = None
+    rules = None
+    def __init__(self,line):
+        self.name, rules = line.strip('}').split('{')
+        rules = rules.split(',')
+        self.rules = [Rule(r) for r in rules]
+    
+    def __call__(self,part:Part):
+        # Set the default rule
+        matched_rule = self.rules[-1]
+        for rule in self.rules:
+            matched = rule(part)
+            if matched:
+                matched_rule = rule
+                break
+        
+        # We now have the matched rule. Send up it's action.
+        return matched_rule.action
+
+class Rule:
+    part = None
+    operator = None
+    value = None
+    action = None
+    def __init__(self,line):
+        if ':' not in line:
+            self.action = line
+        else:
+            self.part = line[0]
+            self.operator = line[1]
+            self.value = int(line[2:line.index(':')])
+            self.action = line[line.index(':')+1:]
+    
+    def __repr__(self):
+        if self.part:
+            return f'({self.part} {self.operator} {self.value}) -> {self.action}'
+        else:
+            return f'--> {self.action}'
+    
+    def __call__(self, part:Part):
+        # If we were not given a rule to work with, we are a default rule and so always evaluate true.
+        if not self.part:
+            return True
+
+        matched = False
+        if self.operator == '>':
+            matched = getattr(part,self.part) > self.value
+        else:
+            matched = getattr(part,self.part) < self.value
+        
+        if matched:
+            if self.action in part.visited:
+                matched = False
+
+        return matched
---
+++ 2023/19/test.py
@@ -0,0 +1,72 @@
+import unittest
+from main import Part, Workflow, Rule
+
+class TestPart(unittest.TestCase):
+    def test_line_parse(self):
+        line = '{x=787,m=2655,a=1222,s=2876}'
+        part = Part(line)
+        self.assertEqual(part.x,787)
+        self.assertEqual(part.m,2655)
+        self.assertEqual(part.a,1222)
+        self.assertEqual(part.s,2876)
+        self.assertEqual(part.visited,set())
+
+class TestWorkflow(unittest.TestCase):
+    def test_workflow_parse(self):
+        line = 'px{a<2006:qkq,m>2090:A,rfg}'
+        wf = Workflow(line)
+        self.assertEqual(wf.name, 'px')
+        self.assertEqual(len(wf.rules), 3)
+    
+    def test_call_workflow(self):
+        wf = Workflow('px{a<2006:qkq,m>2090:A,rfg}')
+        p1 = Part('{x=787,m=255,a=222,s=2876}')
+        p2 = Part('{x=787,m=2665,a=6222,s=2876}')
+        p3 = Part('{x=787,m=255,a=6222,s=2876}')
+
+        self.assertEqual(wf(p1),'qkq')
+        self.assertEqual(wf(p2),'A')
+        self.assertEqual(wf(p3),'rfg')
+
+class TestRule(unittest.TestCase):
+    def test_rule_parse(self):
+        line = 'a<2006:qkq'
+        r = Rule(line)
+        self.assertEqual(r.part, 'a')
+        self.assertEqual(r.operator, '<')
+        self.assertEqual(r.value, 2006)
+        self.assertEqual(r.action, 'qkq')
+
+        shortline = 'A'
+        r = Rule(shortline)
+        self.assertEqual(r.action,'A')
+        self.assertIsNone(r.part)
+        self.assertIsNone(r.operator)
+        self.assertIsNone(r.value)
+    
+    def test_call_rule(self):
+        line = '{x=787,m=2655,a=1222,s=2876}'
+        part = Part(line)
+
+        r1 = Rule('x<2006:r1')
+        r2 = Rule('m>206:r2')
+        r3 = Rule('a<648:r3')
+        r4 = Rule('s>1155:r4')
+        r5 = Rule('A')
+
+        self.assertTrue( r1(part))
+        self.assertTrue( r2(part))
+        self.assertFalse(r3(part))
+        self.assertTrue( r4(part))
+        self.assertTrue( r5(part))
+
+        part.visited = {'r1','r2','r3'}
+
+        self.assertFalse( r1(part))
+        self.assertFalse( r2(part))
+        self.assertFalse(r3(part))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
