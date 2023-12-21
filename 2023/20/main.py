f1ac2d436dc22f15e4dce643bcb7d722cfb48eae
Toby Archer <Toben.Archer@CovenantEyes.com>
2023-12-20 21:41:58 -0600

    close, but not quite there. time for bed. 55min in.

--- 2023/20/main.py
+++ 2023/20/main.py
@@ -1,12 +1,53 @@
-class FlipFlop():
+class CMod():
     def __init__(self,line):
-        self.name = line[1:3]
+        self.name = line[1:3].strip(' ')
         self.outputs = line[7:].split(', ')
+
+class FlipFlop(CMod):
+    def __init__(self,line):
+        super().__init__(line)
         self.state = False
     
     def signal(self,signal):
-        if signal == False:
+        if signal[1] == False:
             self.state = not self.state
-            return [(output,self.state) for output in self.outputs]
+            return [(self.name,self.state,output) for output in self.outputs]
         else:
             return []
+
+class Conjunction(CMod):
+    def __init__(self,line):
+        super().__init__(line)
+        self.memory = {}
+    
+    def signal(self,signal):
+        self.memory[signal[0]] = signal[1]
+        allhigh = all([self.memory[i] for i in self.memory])
+        if allhigh:
+            return [(self.name,False,output) for output in self.outputs]
+        else:
+            return [(self.name,True,output) for output in self.outputs]
+
+if __name__ == '__main__':
+    lines = open('input').read().split('\n')
+    ffs = [FlipFlop(line) for line in lines if '%' in line]
+    cons = [Conjunction(line) for line in lines if '&' in line]
+    b_line = [line for line in lines if 'broadcaster' in line][0]
+    initial_signals = [('br',False,i) for i in b_line.split(' -> ')[1].split(', ')]
+    mods = {i.name:i for i in ffs+cons}
+    high_sigs = 0
+    low_sigs = 0
+    for i in range(1):
+        signals = [i for i in initial_signals]
+        while len(signals):
+            print(f'Current signal queue: {len(signals)}  sig_count {high_sigs,low_sigs}',end='\r')
+            signal = signals.pop(0)
+            if signal[1]:
+                high_sigs +=1
+            else:
+                low_sigs += 1
+            new_signals = mods[signal[2]].signal(signal)
+            signals = signals + new_signals
+            print(new_signals, signals)
+    print()
+    print(high_sigs*low_sigs)
--- 2023/20/test.py
+++ 2023/20/test.py
@@ -1,38 +1,69 @@
 import unittest
-from main import FlipFlop
+from main import FlipFlop, Conjunction
 
 class TestFlipflow(unittest.TestCase):
     def test_flipflop_parse(self):
-        line = '&sk -> rg, hh, hv, kr, kh, zl, zn'
+        line = '%sk -> rg, hh, hv, kr, kh, zl, zn'
         ff = FlipFlop(line)
         self.assertEqual(ff.name,'sk')
         self.assertEqual(ff.outputs,['rg','hh','hv','kr','kh','zl','zn'])
         self.assertEqual(ff.state,False)
     
     def test_handle_signal(self):
-        line = '&sk -> rg, hh'
+        line = '%sk -> rg, hh'
         ff = FlipFlop(line)
 
         self.assertEqual(ff.state,False)
-        new_signals = ff.signal(True)
+        new_signals = ff.signal(('sk',True,'rg'))
         self.assertEqual(ff.state,False)
         self.assertEqual(len(new_signals), 0)
 
-        new_signals = ff.signal(False)
+        new_signals = ff.signal(('sk',False,'rg'))
         self.assertEqual(ff.state,True)
         self.assertEqual(len(new_signals), 2)
-        self.assertEqual(new_signals[0],('rg',True))
-        self.assertEqual(new_signals[1],('hh',True))
+        self.assertEqual(new_signals[0],('sk',True,'rg'))
+        self.assertEqual(new_signals[1],('sk',True,'hh'))
 
-        new_signals = ff.signal(True)
+        new_signals = ff.signal(('sk',True,'rg'))
         self.assertEqual(ff.state,True)
         self.assertEqual(len(new_signals), 0)
 
-        new_signals = ff.signal(False)
+        new_signals = ff.signal(('sk',False,'rg'))
         self.assertEqual(ff.state,False)
         self.assertEqual(len(new_signals), 2)
-        self.assertEqual(new_signals[0],('rg',False))
-        self.assertEqual(new_signals[1],('hh',False))
+        self.assertEqual(new_signals[0],('sk',False,'rg'))
+        self.assertEqual(new_signals[1],('sk',False,'hh'))
+
+class TestConjuction(unittest.TestCase):
+    def test_conjuction_parse(self):
+        line = '&sk -> rg, hh, hv, kr, kh, zl, zn'
+        c = Conjunction(line)
+        self.assertEqual(c.name,'sk')
+        self.assertEqual(c.outputs,['rg','hh','hv','kr','kh','zl','zn'])
+
+    def test_conjuction_signal(self):
+        line = '%sk -> rg, hh'
+        c = Conjunction(line)
+
+        ns = c.signal(('i1',False,'rg'))
+        self.assertEqual(ns[0],('sk',True,'rg'))
+        self.assertEqual(ns[1],('sk',True,'hh'))
+
+        ns = c.signal(('i1',True,'rg'))
+        self.assertEqual(ns[0],('sk',False,'rg'))
+        self.assertEqual(ns[1],('sk',False,'hh'))
+
+
+        ns = c.signal(('i1',False,'rg'))
+        self.assertEqual(ns[0],('sk',True,'rg'))
+        self.assertEqual(ns[1],('sk',True,'hh'))
+        ns = c.signal(('i2',True,'sk'))
+        self.assertEqual(ns[0],('sk',True,'rg'))
+        self.assertEqual(ns[1],('sk',True,'hh'))
+        ns = c.signal(('i1',True,'rg'))
+        self.assertEqual(ns[0],('sk',False,'rg'))
+        self.assertEqual(ns[1],('sk',False,'hh'))
+        
 
 if __name__ == '__main__':
     unittest.main()
