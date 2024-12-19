d1ac2c7b3c6975afadc539ef81c7e7b489eeb8b5
toby <toby@studiosleepygiraffe.com>
2024-12-18 19:54:10 -0600

    scanning two directions works.

--- 2024/04/main.c
+++ 2024/04/main.c
@@ -48,9 +48,54 @@ int scan_horizontal() {
     return count;
 }
 
+int scan_vertical() {
+    int ftrack = 0;
+    int rtrack = 0;
+    int count = 0;
+    for(int i=0; i<HEIGHT; i++) {
+        for(int j=0; j<WIDTH; j++) {
+            if (grid[j][i] == forward[ftrack]){
+                printf("Found a potential XMAS at %dx%d\n", j, i);
+                ftrack++;
+            } else {
+                if (grid[j][i] == forward[0])
+                    ftrack = 1;
+                else
+                    ftrack = 0;
+            }
+
+            if (ftrack == 4) {
+                printf("XMAS found by %d,%d\n", j, i);
+                count++;
+                ftrack = 0;
+            }
+
+            if (grid[j][i] == reverse[rtrack]){
+                printf("Found a potential SMAX at %dx%d\n", j, i);
+                rtrack++;
+            } else {
+                if (grid[j][i] == reverse[0]){
+                    rtrack = 1;
+                } else {
+                    rtrack = 0;
+                }
+            }
+
+            if (rtrack == 4) {
+                printf("SMAX found by %d,%d\n", j, i);
+                count++;
+                rtrack = 0;
+            }
+        }
+    }
+    return count;
+
+}
+
 int main() {
     int count = 0;
     count += scan_horizontal();
+    count += scan_vertical();
 
     printf("Count is: %d\n", count);
 }
